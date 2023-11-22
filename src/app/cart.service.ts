974413ed6c408f402d79e237eee84960669065c7
Lennon Martins da Silva <lennon.silva@PMCG.IMTI>
2023-11-22 16:50:31 -0400

    Primeira versão.

---
+++ src/app/cart.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  constructor(
+    private httpcliente: HttpClient
+  ){}
+
+  items: Product[] = new Array<Product>();
+
+  addToCart(product : Product){
+    this.items.push(product);
+  }
+
+  getItems(){
+    return this.items;
+  }
+
+  clearCart(){
+    this.items = new Array<Product>();
+    return this.items;
+  }
+
+  getShippingPrices(){
+    return this.httpcliente.get('/assets/shipping.json')
+      .pipe(map((data) =>{
+        return data.map((shippingPrice) => ({
+          type: shippingPrice.type,
+          price: shippingPrice.price,
+        }));
+      }));
+  }
+}
