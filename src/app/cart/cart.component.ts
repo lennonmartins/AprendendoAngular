9e2ddb99dfc1f3102abf80a08d27e0257e0e6c5c
Lennon Martins da Silva <lennon.silva@PMCG.IMTI>
2023-11-23 08:54:22 -0400

    Inclusão do form para finalização do processo de compras.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -10,8 +11,20 @@ import { CartService } from '../cart.service';
 export class CartComponent {
 
   constructor(
-    private cartService: CartService
-  ){ }
-  
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+  ) { }
+
   items = this.cartService.getItems();
+
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
+  onSubmit(){
+    this.items = this.cartService.clearCart()
+    console.warn('Seu pedido foi enviado!', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
